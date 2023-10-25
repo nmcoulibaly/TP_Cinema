d43c579054b0644478e2ca2ef7fe8dc9f1d8698b
nmcoulibaly <70775525+nmcoulibaly@users.noreply.github.com>
2023-10-25 23:52:13 +0200

    models file

--- Films.js
+++ Films.js
@@ -1,42 +1,31 @@
-import { Card, Button, Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router";
-
-function Films() {
-    const [films, setMovies] = useState([]);
-
-    useEffect(() => {
-        axios.get('http://localhost:8080/api/films/getFilms').then(resp => {
-            setMovies(resp.data);
-            console.log(resp.data);
-        }).catch(er => {
-            alert(er.message);
-        });
-    }, []);
-    return (<>
-        <div className="home">
-            <div className="featured-movies">
-                <h1>Liste de Films</h1>
-                <Row>
-                    {films.map((movie, index) => (
-                        <Col key={index} md={4}>
-                            <div className="text-center"><br />
-                                <h4>{movie.title}</h4>
-                                <img className="fit-picture" src="/assets/images/film.jpg" style={{ width: '10rem' }} alt={movie.title} /><br /><br />
-                                <Link to={`/detail/${movie._id}`} className="btn btn-primary">
-                                    Détails
-                                </Link>
-                            </div>
-                        </Col>
-                    ))}
-                </Row>
-            </div>
-        </div>
-        <footer>
-            <p>Contactez-nous : contact@appfilms.com</p>
-        </footer>
-    </>)
-}
-export default Films;
+const mongoose = require('mongoose');
+const filmSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+    },
+    title: {
+        type: String,
+    },
+    year: {
+        type: Number,
+    },
+    genre: {
+        type: String,
+    },
+    image: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    acteurs_principaux: {
+        type: Array
+    },
+    date_de_sortie: {
+        type: String
+    },
+    realisateur: {
+        type: String
+    }
+})
+module.exports = Films = mongoose.model('Films', filmSchema, 'films')
